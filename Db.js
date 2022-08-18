67927b4f85d6e9a1144918eef92ee282792266dd
Jack Lu <jacklu@jacklu.me>
2022-08-18 22:59:21 +0800

    Db: +index

--- Db.js
+++ Db.js
@@ -5,8 +5,12 @@ const sqlite = require('sqlite');
 async function Db(filename) {
 	var exists = fs.existsSync(filename);
 	var db = await sqlite.open({ filename: filename, driver: sqlite3.Database });
-	if (!exists)
+	if (!exists) {
 		await db.run("CREATE TABLE message (talker TEXT, type INTEGER, text TEXT, time DATETIME)");
+		await db.run("CREATE INDEX message_talker ON message (talker)");
+		await db.run("CREATE INDEX message_type ON message (type)");
+		await db.run("CREATE INDEX message_time ON message (time)");
+	}
 	return db;
 }
 
